46b4cc3cfb6eaf573e1cc29d13817ecb5c58e37e
alena <108529764+AlenaMushko@users.noreply.github.com>
2024-12-26 11:32:45 +0200

    create bucket

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,10 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { ValidationPipe } from "@nestjs/common";
 
 import { SwaggerHelper } from './common/helpers/swagger.helper';
+import { AppConfig, Config } from './config/config.type';
 import { AppModule } from './modules/app.module';
 
 async function bootstrap() {
@@ -31,12 +33,21 @@ async function bootstrap() {
     },
   });
 
-  app.useGlobalPipes(new ValidationPipe({
-    transform: true,
-    whitelist: true,
-    forbidNonWhitelisted: true,
-    }
-  ));
-  await app.listen(3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const configService = app.get(ConfigService<Config>);
+  const appConfig = configService.get<AppConfig>('app');
+
+  await app.listen(appConfig.port, () => {
+    const url = `http://${appConfig.host}:${appConfig.port}`;
+    Logger.log(`Server is running on ${url}`);
+    Logger.log(`Swagger is running on ${url}/docs`);
+  });
 }
 bootstrap();
--- backend/src/modules/app.module.ts
+++ backend/src/modules/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
+import configuration from '../config/configs';
 import { HealthModule } from './health/health.module';
+import { PostgresModule } from './postgres/postgres.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule, HealthModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: './environments/local.env',
+      load: [configuration],
+      isGlobal: true,
+    }),
+    UserModule,
+    HealthModule,
+    PostgresModule,
+  ],
   controllers: [],
   providers: [],
 })
